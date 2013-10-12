4d24963d21e944fc7514ccb08071e52ce1183dee
Aled Sage <aled.sage@gmail.com>
2013-10-12 23:47:14 +0100

    Fix setup-server.sh
    - make it look like what is actually being used on the test box!

--- setup-server.sh
+++ setup-server.sh
@@ -1,6 +1,6 @@
-echo ============================================================================================================
-echo  CUSTOMIZING HOST: this script requires ${defaultGateway}, ${destination} and ${originalGateway} properties
-echo ============================================================================================================
+echo ==============================================================================
+echo  CUSTOMIZING HOST
+echo ==============================================================================
 
 unset OS
 OS=`head -1 /etc/issue | awk '{ print $1 }'`
@@ -15,16 +15,23 @@ echo Inserting nameserver 8.8.8.8 to /etc/resolv.conf
 sudo -s -- '/bin/echo nameserver 8.8.8.8 >> /etc/resolv.conf'
 
 echo Changing default gateway to ${defaultGateway}
-/sbin/route -n
-sudo -s -- '/bin/touch /etc/sysconfig/network-scripts/route-eth0'
-sudo -s -- '/bin/echo GATEWAY=${defaultGateway} >> /etc/sysconfig/network-scripts/ifcfg-eth0'
-sudo -s -- '/bin/echo ${destination} via ${originalGateway} dev eth0 >> /etc/sysconfig/network-scripts/route-eth0'
+#sudo -s -- '/sbin/route del default gw ${originalGateway}'
+#sudo -s -- '/sbin/route add default gw ${defaultGateway} eth0'
+#sudo -s -- '/sbin/ip route add ${destination} via ${originalGateway} dev eth0'
+#sudo -s -- '/bin/touch /etc/sysconfig/network-scripts/route-eth0'
+
+# set default gateway (our private proxy machine) if we can't already contact the internet
+# (if a public ip was set up, with public network gateway, then we can already contact the internet,
+# and running this command would break INGRESS to the public ip)
+( ping -c 1 8.8.8.8 ) || ( sudo -s -- '/bin/echo GATEWAY=${defaultGateway} >> /etc/sysconfig/network' && sudo -s -- '/bin/echo ${destination} via ${originalGateway} dev eth0 >> /etc/sysconfig/network-scripts/route-eth0' )
+
 sudo -s -- '/sbin/service network restart'
 /sbin/route -n
 
 echo Shutdown iptables
 sudo -s -- '/sbin/service iptables stop'
 sudo -s -- '/sbin/chkconfig iptables off'
+/sbin/chkconfig
 
 echo ==============================================================================
 echo  CUSTOMIZATION COMPLETED
