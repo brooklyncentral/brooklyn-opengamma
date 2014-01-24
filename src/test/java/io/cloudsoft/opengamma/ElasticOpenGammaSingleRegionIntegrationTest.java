9fd64b0a542cfdaf044979bb04677da72e69cba6
Andrea Turli <andrea.turli@gmail.com>
2014-01-24 16:37:35 +0100

    address more comments

--- src/test/java/io/cloudsoft/opengamma/ElasticOpenGammaSingleRegionIntegrationTest.java
+++ src/test/java/io/cloudsoft/opengamma/ElasticOpenGammaSingleRegionIntegrationTest.java
@@ -1,5 +1,6 @@
 package io.cloudsoft.opengamma;
 
+import brooklyn.entity.Entity;
 import brooklyn.entity.basic.ApplicationBuilder;
 import brooklyn.entity.basic.Entities;
 import brooklyn.entity.database.postgresql.PostgreSqlNode;
@@ -8,11 +9,16 @@ import brooklyn.entity.messaging.activemq.ActiveMQBroker;
 import brooklyn.entity.proxying.EntitySpec;
 import brooklyn.entity.trait.Startable;
 import brooklyn.entity.webapp.ControlledDynamicWebAppCluster;
+import brooklyn.entity.webapp.ControlledDynamicWebAppClusterImpl;
 import brooklyn.location.basic.LocalhostMachineProvisioningLocation;
 import brooklyn.test.Asserts;
 import brooklyn.test.EntityTestUtils;
 import brooklyn.test.entity.TestApplication;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
+import com.google.inject.internal.util.$AbstractMapEntry;
+import io.cloudsoft.opengamma.app.ClusteredOpenGammaApplication;
+import io.cloudsoft.opengamma.app.ElasticOpenGammaApplication;
 import io.cloudsoft.opengamma.server.OpenGammaMonitoringAggregation;
 import io.cloudsoft.opengamma.server.OpenGammaServer;
 import io.cloudsoft.opengamma.server.SimulatedExamplesServer;
@@ -26,13 +32,15 @@ import static org.testng.Assert.assertFalse;
 
 public class ElasticOpenGammaSingleRegionIntegrationTest {
 
-    private TestApplication app;
+    private ClusteredOpenGammaApplication app;
     private LocalhostMachineProvisioningLocation localhostProvisioningLocation;
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
         localhostProvisioningLocation = new LocalhostMachineProvisioningLocation();
-        app = ApplicationBuilder.newManagedApp(TestApplication.class);
+        //app = ApplicationBuilder.newManagedApp(ClusteredOpenGammaApplication.class);
+        app = ApplicationBuilder.newManagedApp(EntitySpec.create(ClusteredOpenGammaApplication.class)
+                .configure(ClusteredOpenGammaApplication.SUPPORT_MULTIREGION, false));
     }
 
     @AfterMethod(alwaysRun = true)
@@ -42,41 +50,40 @@ public class ElasticOpenGammaSingleRegionIntegrationTest {
 
     @Test(groups = "Integration")
     public void testCanStartAndStop() throws Exception {
-        ActiveMQBroker broker = app.createAndManageChild(EntitySpec.create(ActiveMQBroker.class));
-        PostgreSqlNode database = app.createAndManageChild(EntitySpec.create(PostgreSqlSpecs.spec()
-                .configure(PostgreSqlNode.CREATION_SCRIPT_URL, "classpath:/io/cloudsoft/opengamma/config/create-brooklyn-db.sql")
-                .configure(PostgreSqlNode.DISCONNECT_ON_STOP, true)));
-        EntitySpec<ControlledDynamicWebAppCluster> web = EntitySpec.create(ControlledDynamicWebAppCluster.class)
-                .displayName("Load-Balanced Cluster")
-                .configure(ControlledDynamicWebAppCluster.INITIAL_SIZE, 1)
-                .configure(ControlledDynamicWebAppCluster.MEMBER_SPEC,
-                        EntitySpec.create(SimulatedExamplesServer.class)
-                                .displayName("OpenGamma Server")
-                                .configure(OpenGammaServer.BROKER, broker)
-                                .configure(OpenGammaServer.DATABASE, database));
-
-        final ControlledDynamicWebAppCluster ogWebCluster = app.createAndManageChild(web);
-
-        assertEquals(ogWebCluster.getCurrentSize().intValue(), 0);
+        ControlledDynamicWebAppCluster ogWebCluster = null;
         app.start(ImmutableList.of(localhostProvisioningLocation));
-
-        EntityTestUtils.assertAttributeEqualsEventually(ogWebCluster, Startable.SERVICE_UP, true);
-        assertEquals(ogWebCluster.getCurrentSize().intValue(), 1);
-
-        Asserts.succeedsEventually(new Runnable() {
-            public void run() {
-                assertTrue(ogWebCluster.getAttribute(OpenGammaMonitoringAggregation.PROCESSING_TIME_PER_SECOND_LAST) >= 0);
-                assertTrue(ogWebCluster.getAttribute(OpenGammaMonitoringAggregation.PROCESSING_TIME_PER_SECOND_IN_WINDOW) >= 0);
-                assertTrue(ogWebCluster.getAttribute(OpenGammaMonitoringAggregation.VIEW_PROCESSES_COUNT) >= 0);
-                assertTrue(ogWebCluster.getAttribute(OpenGammaMonitoringAggregation.PROCESS_CPU_TIME_FRACTION_IN_WINDOW) >= 0);
-                assertTrue(ogWebCluster.getAttribute(OpenGammaMonitoringAggregation.PROCESSING_TIME_PER_SECOND_LAST_PER_NODE) >= 0);
-                assertTrue(ogWebCluster.getAttribute(OpenGammaMonitoringAggregation.PROCESSING_TIME_PER_SECOND_IN_WINDOW_PER_NODE) >= 0);
-                assertTrue(ogWebCluster.getAttribute(OpenGammaMonitoringAggregation.VIEW_PROCESSES_COUNT_PER_NODE) >= 0);
-                assertTrue(ogWebCluster.getAttribute(OpenGammaMonitoringAggregation.PROCESS_CPU_TIME_FRACTION_IN_WINDOW_PER_NODE) >= 0);
+        Entities.dumpInfo(app);
+        for (Entity child : app.getChildren()) {
+            if(child instanceof ControlledDynamicWebAppCluster) {
+                ogWebCluster = (ControlledDynamicWebAppCluster) child;
             }
-        });
+
+        }
+        EntityTestUtils.assertAttributeEqualsEventually(ogWebCluster, Startable.SERVICE_UP, true);
+        assertEquals(ogWebCluster.getCurrentSize().intValue(), 2);
+
+        Asserts.succeedsEventually(new CheckControlledDynamicWebAppClusterStatus(ogWebCluster));
         ogWebCluster.stop();
         assertFalse(ogWebCluster.getAttribute(Startable.SERVICE_UP));
     }
 
+    private class CheckControlledDynamicWebAppClusterStatus implements Runnable {
+
+        private ControlledDynamicWebAppCluster ogWebCluster;
+
+        public CheckControlledDynamicWebAppClusterStatus(ControlledDynamicWebAppCluster ogWebCluster) {
+            this.ogWebCluster = ogWebCluster;
+        }
+
+        public void run() {
+            assertTrue(ogWebCluster.getAttribute(OpenGammaMonitoringAggregation.PROCESSING_TIME_PER_SECOND_LAST) >= 0);
+            assertTrue(ogWebCluster.getAttribute(OpenGammaMonitoringAggregation.PROCESSING_TIME_PER_SECOND_IN_WINDOW) >= 0);
+            assertTrue(ogWebCluster.getAttribute(OpenGammaMonitoringAggregation.VIEW_PROCESSES_COUNT) >= 0);
+            assertTrue(ogWebCluster.getAttribute(OpenGammaMonitoringAggregation.PROCESS_CPU_TIME_FRACTION_IN_WINDOW) >= 0);
+            assertTrue(ogWebCluster.getAttribute(OpenGammaMonitoringAggregation.PROCESSING_TIME_PER_SECOND_LAST_PER_NODE) >= 0);
+            assertTrue(ogWebCluster.getAttribute(OpenGammaMonitoringAggregation.PROCESSING_TIME_PER_SECOND_IN_WINDOW_PER_NODE) >= 0);
+            assertTrue(ogWebCluster.getAttribute(OpenGammaMonitoringAggregation.VIEW_PROCESSES_COUNT_PER_NODE) >= 0);
+            assertTrue(ogWebCluster.getAttribute(OpenGammaMonitoringAggregation.PROCESS_CPU_TIME_FRACTION_IN_WINDOW_PER_NODE) >= 0);
+        }
+    }
 }
